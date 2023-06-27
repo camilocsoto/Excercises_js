4e5b10d20b67c0dc394a7675ef3b61ec8b4fcaf4
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-06-27 15:31:35 -0500

    conceptos de interpolación, \n y las comillas francesas

---
+++ ecmascript/src/es6/03-string.js
@@ -0,0 +1,17 @@
+let hello = 'lello'
+let world = 'world'
+
+//versiones anteriores de js
+let epicPhrase = hello + ' ' + world
+// console.log(epicPhrase);
+
+//templeate literals
+let epicPhrase2 = `${hello} ${world}`
+console.log(epicPhrase2);
+
+//Multi-Line strings
+let lorem = 'esto es un string \n esto es otra línea'
+
+let lorem2 =`Linea
+otra línea
+otra línea`
