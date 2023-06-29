c20971dc4fda581cb1cd0b92b18f97c0666ec107
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-06-29 16:22:43 -0500

    Concepto de módulos y herencia con node js. Star Wars Theme.

---
+++ ecmascript/src/es6/09-mdl.js
@@ -0,0 +1,5 @@
+//determinar si es un sith o un jedi
+const typeOfPort =() => {
+    console.log('It is a jedi');
+}
+export default typeOfPort
---
+++ ecmascript/src/es6/09-module.js
@@ -0,0 +1,5 @@
+import typeOfPort from "./09-mdl.js";
+
+typeOfPort()
+
+//En el package.json, se añadió bajo license MIT, el parámetro type:module
