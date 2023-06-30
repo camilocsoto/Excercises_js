09522a3604bfe606c68d3213ab7ca81128bec55a
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-06-30 18:21:29 -0500

    iterar arrays y números con función*, yield, next().value

--- ecmascript/src/es6/10-generators.js
+++ ecmascript/src/es6/10-generators.js
@@ -15,3 +15,26 @@ console.log(it.next().value);
 console.log(it.next().value);
 console.log(it.next().value);
 console.log(it.next().value);
+
+
+
+//reto playground = una tienda de gatos necesita identificar con un código distinto a cada michi sin que se repita todo el tiempo el proceso
+export function* getId() {
+    for (let id = 1; true; id++) { //para que se repita infinitamente
+      yield id 
+    }
+  }
+  const serial = getId()
+  console.log(serial.next().value)
+  
+
+  //forma corta y sencilla
+  export function* getId() {
+    let lastId=1;
+    while (true) {
+      yield lastId++;
+    }
+  }
+  const serial2 = getId()
+  console.log(serial2.next().value)
+  
