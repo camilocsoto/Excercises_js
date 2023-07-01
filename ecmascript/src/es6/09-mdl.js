d2d675c55e505cf69a18fc0d9dfd50bd49880643
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-06-30 19:02:38 -0500

    Concepto de módulos y herencia con node js. Star Wars Theme.

--- ecmascript/src/es6/09-mdl.js
+++ ecmascript/src/es6/09-mdl.js
@@ -3,3 +3,20 @@ const typeOfPort =() => {
     console.log('It is a jedi');
 }
 export default typeOfPort
+
+//Playground project = llamar esta función asincronica al otro módulo
+export async function getData() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([{
+                movie: "El despertar de los michis",
+                year: 2021,
+                protagonist: "Mr. Michi"
+            }, {
+                movie: "101 Michis",
+                year: 2019,
+                protagonist: "Tommy Michiguire"
+            }]);
+        }, 1000);
+    });
+}
--- ecmascript/src/es6/09-module.js
+++ ecmascript/src/es6/09-module.js
@@ -3,3 +3,12 @@ import typeOfPort from "./09-mdl.js";
 typeOfPort()
 
 //En el package.json, se añadió bajo license MIT, el parámetro type:module
+
+// playground lista de películas
+
+import { getData } from './09-mdl.js' //importar objetos {}
+
+export function solution() {
+  return getData()
+}
+
