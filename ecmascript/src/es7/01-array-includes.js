b26bd7735f80c179f1a89a477106384dbd6044bc
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-06-30 23:02:34 -0500

    método includes en arrays y strings

--- ecmascript/src/es7/01-array-includes.js
+++ ecmascript/src/es7/01-array-includes.js
@@ -4,7 +4,8 @@ message.includes('bttle') //false
 
 // Identificar un valor especifico de un array
 const sith= ['Palpatine', 'Maul', 'Bane', 'Doocku']
-console.log(sith.includes('yoda'));
+sith.includes('yoda') //false
+sith[2].includes('Mau')
 
 //Si tiene una mayuscula va a retonar false, es buena práctica usar lowerCase()
 // 
