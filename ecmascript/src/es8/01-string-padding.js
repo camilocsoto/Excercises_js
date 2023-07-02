01821d3c929193dfb4736065999e9871634e5308
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-07-01 22:48:19 -0500

    hace que un string cumpla cierto número de caracteres con string padding

---
+++ ecmascript/src/es8/01-string-padding.js
@@ -0,0 +1,5 @@
+const character = 'thrawn'
+
+//console.log(character.padStart(10, '|°')); //entrega un string de 7 caracteres y se añaden al comienzo
+
+console.log(character.padEnd(10, '|°')); //entrega un string de 7 caracteres y se añaden al final
