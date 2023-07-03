3529244e10be6f0a555e7ee108d40e75877bfe69
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-07-03 00:00:22 -0500

    recortar espacios en blanco de un string

---
+++ ecmascript/src/es10/01-trim-start-end.js
@@ -0,0 +1,8 @@
+/**
+ * trimStart recorta los espacios del comienzo 
+ * trimStart recorta los espacios del final 
+*/ 
+const word = '              hello world                 ';
+console.log(word.trim());
+console.log(word.trimStart());
+console.log(word.trimEnd());
