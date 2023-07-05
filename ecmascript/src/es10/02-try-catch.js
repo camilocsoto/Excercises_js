dd22fe36acdb08fe08a3608649106743a4aa409b
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-07-04 21:44:26 -0500

    Dos formas de usar try catch con analogía al fin del mundo.

---
+++ ecmascript/src/es10/02-try-catch.js
@@ -0,0 +1,14 @@
+try {
+    atomicBomb()
+} catch (error) {
+    // console.log(error);
+}
+
+//caso de uso muy particular, generalmente se usa el otro para manejar más datos
+try {
+    apocalyps()
+} catch {
+    
+    console.log('Its a false alert');
+
+}
