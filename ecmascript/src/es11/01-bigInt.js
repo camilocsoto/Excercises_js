4a416d33567ae78d3bbbe9e28f70e0350b7ff803
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-07-05 23:26:17 -0500

    constructor bigInt() o al final del número poner n

--- ecmascript/src/es11/01-bigInt.js
+++ ecmascript/src/es11/01-bigInt.js
@@ -1,3 +1,8 @@
+//usar depués de 16 cifras, ya que después de estas cifras, hay errores
+const max = Number.MAX_SAFE_INTEGER // 9007199254740991
+const min = Number.MIN_SAFE_INTEGER // -9007199254740991
+
+
 const galaxyPlanet = 10392184932854809n; //way 1
 const peopleTatooine = BigInt(2213455) //way 2: constructor bigInt
 console.log(galaxyPlanet);
