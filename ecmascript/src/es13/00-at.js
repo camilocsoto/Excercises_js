39448f232f5e617c4da7e83d4d9cd1cdadfb9195
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-07-10 01:31:31 -0500

    at es una alternativa al lenght

---
+++ ecmascript/src/es13/00-at.js
@@ -0,0 +1,7 @@
+//supon que este array viene de un api y no sabes cuántas posiciones tiene porque cambia constantemente
+const ligtsaber = ['red', 'purple', 'green', 'blue', 'yellow'] 
+
+console.log(ligtsaber[ligtsaber.length-1]); //devuelve la última posición
+
+//at
+console.log(ligtsaber.at(-4));
