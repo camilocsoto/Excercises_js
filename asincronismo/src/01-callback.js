5b585b1b9bebeb52d4e7312ed48da44ad64fb248
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-07-21 00:38:15 -0500

    pasos para invocar un callback

---
+++ asincronismo/src/01-callback.js
@@ -0,0 +1,12 @@
+function venators(troopers, walkings) {
+    return troopers + walkings; // función normal
+}
+function totalUnits(troopers, walkings, functionVenators) { // callback
+    return functionVenators(troopers, walkings) //recibe como argumento a una función
+}
+/** para invocar el callback:
+ * la función que va hacer callback, debe tener un argumento para la otra función
+ * no se le ponen los parentesis a la función a invocar para no ejecutarla de inmediato
+ * no es necesario pasarle los argumentos a venators, para eso es el callback
+ */
+console.log(totalUnits(501, 200, venators)); 
