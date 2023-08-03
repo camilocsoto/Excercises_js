381512f7321e55aedc680f1f0fdb5c9c823f675d
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-08-02 21:22:12 -0500

    concept of generators

---
+++ asincronismo/src/generator/00-generators.js
@@ -0,0 +1,27 @@
+function* gen() { //los generators permiten pasusar o reanudar un proceso
+    yield 1; //steps.
+    yield 2;
+    yield 3;
+    yield 4;
+}
+//acceder a cada step
+const g = gen()
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+
+
+// otro
+function* iterable (array) {
+    for (let value of array) {
+        yield value
+    }
+}
+const it = iterable(['imperial ship','venator', 'x-wings', 'tie fighters'])
+
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+
