2a58ad15921f3fb49fc31c201ecc98f9bcb5cbe4
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-08-08 12:27:57 -0500

    funciones que con promesas podemos crear async-await y un playground

--- asincronismo/src/async/00-function.js
+++ asincronismo/src/async/00-function.js
@@ -13,3 +13,20 @@ const times = async () => {
 times()
 console.log('---------');
 // console.log(times()); Promise { <pending> }
+
+//playground: dependiendo de si una url está bien o mal: generar una función que la valide.
+const API = 'https://api.escuelajs.co/api/v1/products'
+async function runCode(url) {
+    try {
+      new URL(url);
+      const response = await fetch(url);
+      return response.json();
+     } catch(error) {
+       if (error.message === "Failed to construct 'URL': Invalid URL"){
+         throw new Error('Invalid URL');
+       } else {
+         throw new Error('Something was wrong');
+       }
+     }
+  }
+  runCode(API)
