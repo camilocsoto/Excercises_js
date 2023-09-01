715b7eef4703441978cda2616842a744d803ee90
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-09-01 12:09:03 -0500

    Proyecto terminado

--- websocket/public/js/code.js
+++ websocket/public/js/code.js
@@ -1,29 +1,26 @@
 let socket = io() //variable del server.js - conexión directa entre el servidor y la api del socket- Back
 let statusServer
-let messageServer
+let respServer
+let serverMessage = document.getElementById('serverMessage');
 let p_status = document.getElementById('status_server')
 // métodos: on -> escuchar. emit ->envíar
 socket.on('connect', () => { //metodo para validar que la conexión sea correcta
-    console.log("Enable conection.");
     statusServer = null || 'Enable conection. 🟢';
 })
 socket.on('disconnect', () => { //metodo para validar que la conexión sea correcta
-    console.log("Lost conection.");
-    statusServer = null || 'Lost conection. 🟠';
+    statusServer = 'Lost conection. 🟠';
 })
 socket.emit('enviarMensaje', { //nombre de la variable y el json para enviar info al server
-    usuario: 'Admin',
-    mensaje: `-------------Para comunicarte con el servidor----------------
-    socket.emit('enviarMensaje', { 
-        usuario: 'Admin',
-        mensaje: 'hola mundo'
-    }
-    -------------------------------------------------------------------------`
+    usuario: '<Front>',
+    mensaje: `todo listo para empezar.`
 });
-messageServer = socket.on('enviarMensaje', (mensaje) => { //nombre de la variable y el json para escuchar info del server
-    console.log('servidor: ',mensaje );
+
+socket.on('enviarMensaje', (mensaje) => { //nombre de la variable y el json para escuchar info del server
+        serverMessage.innerHTML = `servidor: ${JSON.stringify(mensaje)}`; //mandar info al front
 })
+
+
+
 setInterval(() => {
 p_status.innerHTML = statusServer;
-console.log(statusServer);
 },2000)
