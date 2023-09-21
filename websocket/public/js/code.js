9b1d8674f8cfca8929488004352fb79c31eb4eaf
Juan Camilo Suárez <ccamsoto@gmail.com>
2023-09-21 09:08:43 -0500

    se mejora la documentacion y se añade un form para que el usuario pueda envíar mensajes al servidor

--- websocket/public/js/code.js
+++ websocket/public/js/code.js
@@ -1,8 +1,9 @@
-let socket = io() //variable del server.js - conexión directa entre el servidor y la api del socket- Back
-let statusServer
-let respServer
-let serverMessage = document.getElementById('serverMessage');
-let p_status = document.getElementById('status_server')
+const socket = io() //objeto del server.js - conexión directa entre el servidor y la api del socket- Back
+let statusServer //mensaje que indica la conexión del servidor para luego ser impresa en el HTML
+const serverMessage = document.getElementById('serverMessage'); //imprime en el html los mensajes del servidor.
+const p_status = document.getElementById('status_server') //párrafo del html para imprimir serverMessage.
+const button = document.getElementById('boton')//trae el botón del html
+button.addEventListener('click', enviarForm) //cada vez que se haga click en el botón, ejecute la función enviarForm
 // métodos: on -> escuchar. emit ->envíar
 socket.on('connect', () => { //metodo para validar que la conexión sea correcta
     statusServer = null || 'Enable conection. 🟢';
@@ -11,12 +12,21 @@ socket.on('disconnect', () => { //metodo para validar que la conexión sea corre
     statusServer = 'Lost conection. 🟠';
 })
 socket.emit('enviarMensaje', { //nombre de la variable y el json para enviar info al server
-    usuario: '<Front>',
+    usuario: '<Front>', //atributos del objeto envíar mensaje
     mensaje: `todo listo para empezar.`
 });
+function enviarForm(event) {
+    const username =document.getElementById('user') //declarar variables
+    const textMessage =document.getElementById('message') //scope local, más seguridad.
+    socket.emit('enviarMensaje', { //nombre de la variable y el json para enviar info al server
+        usuario: username.value, //asignar a los atributos del objeto, los valores que están en el input del formulario
+        mensaje: textMessage.value
+    });
+    event.preventDefault(); //evitar que se actualice la página cada vez que se envíe un mensaje.
+}
 socket.on('enviarMensaje', (mensaje) => { //nombre de la variable y el json para escuchar info del server
-        serverMessage.innerHTML = `servidor: ${ JSON.stringify(mensaje)}`; //mandar info al front
+        serverMessage.innerHTML = `servidor: ${ JSON.stringify(mensaje)}`; //recibir información del servidor e imprimirlo en el html
 })
-setInterval(() => {
-p_status.innerHTML = statusServer;
+setInterval(() => { //insertar un intervalo de tiemppo
+p_status.innerHTML = statusServer; //se actualiza cada 2 segundos la variable statusServer y por consiguiente el HTML
 },2000)
--- websocket/server/socket.js
+++ websocket/server/socket.js
@@ -1,14 +1,14 @@
-const {io} = require('./server') //exporta el objeto
+const {io} = require('./server') //exporta el objeto del server
 io.on('connection', (client)=> { //evalúa la conexión del cliente.
-    console.log('connected user');
-        client.emit('enviarMensaje', {
-            usuario: 'Admin',
+    console.log('connected user'); //si hay un cliente conectado, imprime en la consola del servidor connected user
+        client.emit('enviarMensaje', { //envía un mensaje con el objeto enviarMensaje
+            usuario: 'Admin', //dentro del objeto hay un .json que contiene los atributos del objeto.
             mensaje: 'Bienvenido al socket'
         })
-            client.on('disconnect', () => { // atributo client es para tomar el id del cliente
-                console.log('disconnected user');
+            client.on('disconnect', () => { // atributo client nos lo manda el server para tomar el id del cliente
+                console.log('disconnected user'); //si el cliente se ha desconectado, imprime en la consola del servidor
             })
                 client.on('enviarMensaje', (mensaje) => { //RECIBIR INFORMACION DEL CLIENTE: variable y atributo a recibir
-                    console.log(mensaje);
+                    console.log(mensaje); //imprime en la consola el mensaje del cliente. No hay necesidad de parsearlo.
                 })
 })
